60fe125a7c9c0b09318cd085d89b605ed715c2d5
jackiestchen <jackiestchen@gmail.com>
2021-12-12 12:52:29 -0700

    Fix duration error

--- 4_queries/3_assistance_request_data.sql
+++ 4_queries/3_assistance_request_data.sql
@@ -1,4 +1,4 @@
-SELECT teachers.name as teacher_name, students.name as student_name, assignments.name as assignment, (completed_at - created_at) as duration
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
