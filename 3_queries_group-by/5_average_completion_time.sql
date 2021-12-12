b9c5ee5d7f55bdbc6a05ceae65eb58d9f0f5a136
jackiestchen <jackiestchen@gmail.com>
2021-12-12 00:22:57 -0700

    Fix syntax error

--- 3_queries_group-by/5_average_completion_time.sql
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -1,6 +1,6 @@
 SELECT students.name as student_name, avg(assignment_submissions.duration) as average_assignment_duration
 FROM students
 JOIN assignment_submissions ON students.id = assignment_submissions.student_id
-GROUP BY students.name, students.end_date
-HAVING students.end_date IS NULL
+WHERE students.end_date IS NULL
+GROUP BY students.name
 ORDER BY average_assignment_duration DESC;
