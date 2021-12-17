bba7e34ae396375e4dcc8c3c27798926025a4527
jackiestchen <jackiestchen@gmail.com>
2021-12-16 23:28:23 -0700

    Fetch data from db using node psql

---
+++ bootcamp_app/students.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool
+  .query(
+    `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort_name
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
+    }))
+  })
+  .catch((err) => {
+    console.error("querry error", err.stack);
+  });
